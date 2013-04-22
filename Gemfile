4aff8a42f6a54f15d9e885ea8ba83f8c6e50bcb0
Rob Shaw <rob@borwahs.com>
2013-04-21 23:32:11 -0400

    Add Slim template system with index view.
    - Add slim dependency to Gemfile.
    - Add index view for root requests.
    - Update web class to use Slim templates and load views.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', "~>1.4.2"
+gem 'slim', "~>1.3.8"
--- web.rb
+++ web.rb
@@ -1,5 +1,9 @@
 class Web < Sinatra::Base
+  set :views, "#{File.dirname(__FILE__)}/views"
+
+  set :base_app_title, "Season Ticket Tracker"
+
   get '/' do
-    'Season Ticket Tracker'
+    slim :index, :locals => {:title => "#{settings.base_app_title} | Home"}
   end
 end
