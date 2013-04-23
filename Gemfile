2339ff6c49628a58d82bf7f647da177b30388c30
Rob Shaw <rob@borwahs.com>
2013-04-22 23:39:34 -0400

    Setup database connection to Postgres.
    - Add pg gem for dependency management and requiring.
    - Add before filter to test if database can be connected and then establishing connection.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 gem 'sinatra', "~>1.4.2"
 gem 'slim', "~>1.3.8"
 gem 'sinatra-partial', "~>0.3"
+gem 'pg', "~>0.15"
--- web.rb
+++ web.rb
@@ -5,6 +5,17 @@ class Web < Sinatra::Base
   set :partial_template_engine, :slim
   set :base_app_title, "Season Ticket Tracker"
 
+  before do
+    ping_result = PG::Connection.ping('localhost', 5432, nil, nil, 'stt', nil, nil)
+    is_connectable = ping_result == PG::PQPING_OK ? true : false
+
+    if (!is_connectable)
+      raise "Cannot connect to database"
+    end
+
+    @conn = PG::Connection.new('localhost', 5432, nil, nil, 'stt', nil, nil)
+  end
+
   get '/' do
     slim :index, :locals => {:title => "#{settings.base_app_title} | Home"}
   end
