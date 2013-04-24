d072fa0ffb517422b46e08a3a52ab881936dce8b
Rob Shaw <rob@borwahs.com>
2013-04-23 20:25:00 -0400

    Use hash to set connection arguments.
    - Create and set db_connection_info in Sinatra configuration settings.
    - Only set relevant connection arguments.

--- web.rb
+++ web.rb
@@ -4,16 +4,17 @@ class Web < Sinatra::Base
   set :views, "#{File.dirname(__FILE__)}/views"
   set :partial_template_engine, :slim
   set :base_app_title, "Season Ticket Tracker"
+  set :db_connection_info, { :host => 'localhost', :port => 5432, :dbname => 'stt'}
 
   before do
-    ping_result = PG::Connection.ping('localhost', 5432, nil, nil, 'stt', nil, nil)
+    ping_result = PG::Connection.ping(settings.db_connection_info)
     is_connectable = ping_result == PG::PQPING_OK ? true : false
 
     if (!is_connectable)
       halt 500, slim(:issue, :locals => {:title => "#{settings.base_app_title} | Error"})
     end
 
-    @conn = PG::Connection.new('localhost', 5432, nil, nil, 'stt', nil, nil)
+    @conn = PG::Connection.new(settings.db_connection_info)
   end
 
   get '/' do
