6391f6de2bcfcf3526ba39454d35bfb1ff5ae133
Rob Shaw <rob@borwahs.com>
2013-04-26 00:40:41 -0400

    Use YAML config for db settings.
    - Require YAML gem to load config file.
    - Create Ruby constant for the config file name.
    - Create Ruby constant for the db options key.
    - Modify ping_db_server method to transform conn_opts hash to use the params expected by PG::Connection.ping method.
    - Add Sinatra setting for the base directory.
    - Modify views to use the base directory setting and the File.join method.

--- web.rb
+++ web.rb
@@ -1,10 +1,16 @@
+require 'yaml'
+
 class Web < Sinatra::Base
+  CONFIG_FILE = "config.yml"
+  DB_CONFIG_KEY = "dbopts"
+
   register Sinatra::Partial
 
-  set :views, "#{File.dirname(__FILE__)}/views"
+  set :base_directory, File.dirname(__FILE__)
+  set :views, File.join(settings.base_directory, "/views")
   set :partial_template_engine, :slim
   set :base_app_title, "Season Ticket Tracker"
-  set :db_connection_info, { :host => 'localhost', :port => 5432, :dbname => 'stt'}
+  set :config_opts, YAML::load_file(File.join(settings.base_directory, CONFIG_FILE))
 
   def build_view_options(title, rest={})
     view_options = Hash.new
@@ -18,10 +24,13 @@ class Web < Sinatra::Base
     view_options
   end
 
-  def ping_db_server(connection_options)
+  def ping_db_server(conn_opts)
     value = { :conn_err => "", :can_connect => false }
 
-    ping_result = PG::Connection.ping(connection_options)
+    # transform hash to use expected params by PG gem
+    params = { :host => conn_opts[:host], :dbname => conn_opts[:database] }
+
+    ping_result = PG::Connection.ping(params)
 
     case ping_result
     when PG::PQPING_OK
@@ -39,16 +48,13 @@ class Web < Sinatra::Base
 
   before do
     if @DBconn.nil?
-      ping = ping_db_server(settings.db_connection_info)
+      ping = ping_db_server(settings.config_opts[DB_CONFIG_KEY])
 
       if (!ping[:can_connect])
         halt 500, slim(:issue, :locals => build_view_options("Error", :info => ping[:conn_err]))
       end
 
-      conn_opts = { :host => settings.db_connection_info[:host],
-                    :database => settings.db_connection_info[:dbname] }
-
-      @DBconn = Sequel.postgres(conn_opts)
+      @DBconn = Sequel.postgres(settings.config_opts[DB_CONFIG_KEY])
     end
   end
 
