27a09f410f6e1e5f63c61fac36ad4ec44dc52b34
Rob Shaw <rob@borwahs.com>
2013-04-26 23:27:52 -0400

    Add file to ceate schema via command line.
    - Relies on the config.yml options.

---
+++ schema.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'pg'
+require 'sequel'
+require 'yaml'
+
+configopts = YAML::load_file(File.join(File.dirname(__FILE__), "config.yml"))
+
+Sequel.connect(configopts["dbopts"]) do |db|
+  # create games table
+  if !db.table_exists?(:games)
+    puts "Creating - Games table"
+
+    db.create_table(:games) do
+      primary_key :id
+      String :opponent
+      String :location
+      DateTime :game_time
+      DateTime :last_updated
+    end
+
+    puts "Created - Games table"
+  else
+    puts "Already Created - Games table"
+  end
+end
