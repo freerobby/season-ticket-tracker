b7b59c5d1e6181d36312c18d2d797e3a2d0fdbcf
Rob Shaw <rob@borwahs.com>
2013-05-31 20:13:38 -0400

    Add weekend and weekday filter logic.

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -9,6 +9,8 @@ function Game(data) {
   self.is_day_game = self.datetime.hour() < 15;
   self.is_afternoon_game = self.datetime.hour() > 14 && self.datetime.hour() < 17;
   self.is_night_game = self.datetime.hour() > 16;
+  self.is_weekend_game = self.datetime.day() == 0 || self.datetime.day() == 6;
+  self.is_weekday_game = self.datetime.day() > 0 && self.datetime.day() < 6;
 
   // helper text
   self.game_time_text = self.is_day_game ? " (Day)" : self.is_afternoon_game ? " (Afternoon)" : " (Night)";
@@ -20,26 +22,25 @@ function Game(data) {
 
 function GameViewModel() {
   var self = this;
+
   self.games = ko.observableArray([]);
-  $.getJSON("/games", function(raw) {
+
+  $.getJSON("/games/", function(raw) {
       var games = $.map(raw, function(item) { return new Game(item) });
       self.games(games);
   });
 
-  self.filterGames = function(data, event, type) {
-    if (type == "all")
-      self.typeToShow("all");
-    else if (type == "night")
-      self.typeToShow("night");
-    else if (type == "afternoon")
-    self.typeToShow("afternoon");
-  }
-
   self.typeToShow = ko.observable("all");
+
+  self.filterGames = function(data, event, type) {
+      self.typeToShow(type);
+  }
 
   self.gamesToShow = ko.computed(function() {
       var desiredType = this.typeToShow();
+
       if (desiredType == "all") return this.games();
+
       return ko.utils.arrayFilter(this.games(), function(game) {
           if (desiredType == "day")
           {
@@ -48,6 +49,10 @@ function GameViewModel() {
             return game.is_afternoon_game;
           } else if (desiredType == "night") {
             return game.is_night_game;
+          } else if (desiredType == "weekend") {
+            return game.is_weekend_game;
+          } else if (desiredType == "weekday") {
+            return game.is_weekday_game;
           }
       });
   }, self);
